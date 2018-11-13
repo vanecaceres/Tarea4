ff4b6602f011a1507cafa262152fe4da106a88c5
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-11-12 19:40:27 -0500

    main y def genereal

--- punto2.cpp
+++ punto2.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <fstream> 
+#include<bits/stdc++.h>
+using namespace std;
+
 ///Variebles que guarda los grados con los cuales se alcanza la distanica maxima
 int max_dist=-1;
 int max_dist_angle=-1;
@@ -23,10 +32,30 @@ double ax[steps];
 double ay[steps];
 ///Arreglo para el tiempo
 double T[steps];
+///PI
+double PI = 3.1416;
 //Creo una funcion general que me resuelva de acuerdo a los parametros que le entren, en este caso la masa, coeficiente de friccion, angulo y posiciones iniciales
+//Long double me coge más numeros, mirar luego si long o double
 void solution(double mass,double coefficient_of_friction,double angle,long double Xo,long double Yo,long double Vo){
-
-
+	//Angulo con unidades en radianes para usar sin y cos 
+	double theta=angle*PI/180.0;
+	//Condiciones iniciales
+	x[0]=Xo;
+	y[0]=Yo;
+	T[0]=0;
+	//Desarmo el vector de velocidad en x y y
+	vx[0]=Vo*cos(theta);
+   	vy[0]=Vo*sin(theta);
+	//Desarmo el vector de aceleracion en x y y
+    	ax[0]=-coefficient_of_friction*vx[0]*Vo/mass;
+	//En y mi gravedad afecta, en x no hay gravedad
+    	ay[0]=-gravity-coefficient_of_friction*vy[0]*Vo/mass;
+	//Mi límite es 0, que sería el suelo 
+	int limit=0;
+	///Por el método de leapfrog
+    	for(int i=1;i<steps;i++){
+	
+	}
 }
 int main(){
 
