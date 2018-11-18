97af963cc078cf1f201578a44c5885d4705c9512
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-11-18 09:26:48 -0500

    P3

--- punto3.cpp
+++ punto3.cpp
@@ -7,12 +7,25 @@ using namespace std;
 double k=1.62;
 //Calor especifico
 double Cp=820.0;
-//Densidad
+//Densidad g/cm³ -> k/m³
 double p=2.71*1000;
 //Coeficiente de difusion
 double v=k/(Cp*p);
-//Altura de la calcita
+//Altura de la calcita 0.5m
 double h=0.5;
-//Ancho de la calcita
+//Ancho de la calcita 0.5m
 double w=0.5;
+//Numero de iteraciones
+const int N=80;
+//Tiempo de simulacion maximo
+const int max_time=10000;
+// arreglo que guarda la temperatura en el tiempo i, la fila j y la columna 4k 
+double T[max_time][N][N];
+double dx=h/N;
+double dy=dx;
+double dt=10;
+int tim;
+//Constante de estabilidad
+double fo=(v*dt/(dx*dx));
+
 
