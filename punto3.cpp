d48e2cfc5ee12d003bfa8876ae37cbb46932470b
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-11-18 09:15:31 -0500

    P3

---
+++ punto3.cpp
@@ -0,0 +1,18 @@
+//incluir bibliotecas
+#include<bits/stdc++.h>
+using namespace std;
+///Declaración de constantes
+// Todas las temperaturas tienen unidades K (Kelvin)
+//Conductividad Termica
+double k=1.62;
+//Calor especifico
+double Cp=820.0;
+//Densidad
+double p=2.71*1000;
+//Coeficiente de difusion
+double v=k/(Cp*p);
+//Altura de la calcita
+double h=0.5;
+//Ancho de la calcita
+double w=0.5;
+
