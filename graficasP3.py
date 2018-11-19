22be198fef4e9d4c81fbaaf6ac505b2ccdbbc2af
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-11-18 19:03:21 -0500

    graficas

--- graficasP3.py
+++ graficasP3.py
@@ -1,2 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+input = np.genfromtxt("datos.dat", dtype=None, comments="#", delimiter="\n",unpack = False)
+numeroLinea=0
+nombresFig=[["FronterasFijasInicial.pdf","FronterasFijasAvance1.pdf","FronterasFijasAvance2.pdf","FronterasFijasEq.pdf","FronterasFijasPromedio.pdf"],
+            ["FronterasAbiertasInicial.pdf","FronterasAbiertasAvance1.pdf","FronterasAbiertasAvance2.pdf","FronterasAbiertasEq.pdf","FronterasAbiertasPromedio.pdf"],
+            ["FronterasPeriodicasInicial.pdf","FronterasPeriodicasAvance1.pdf","FronterasPeriodicasAvance2.pdf","FronterasPeriodicasEq.pdfFronterasPeriodicasPromedio.pdf"]]
